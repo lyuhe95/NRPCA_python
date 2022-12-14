3dbd155ff8277645d5221bc6ed0947b32f38090e
He Lyu <lyuhe@msu.edu>
2022-12-14 00:02:09 -0500

    update

--- main.py
+++ main.py
@@ -1,42 +1,164 @@
 import numpy as np
+import random
 from random import choices
+from collections import defaultdict,Counter
 import math
-from utils import *
+import scipy as sp
+from scipy.spatial.distance import cdist
+from utils import get_swiss
+import argparse
+import matplotlib.pyplot as plt
 
 class NRPCA:
     
-    def __init__(self,X,K,num_runs,niter):
+    def __init__(self,X,K,num_runs,niter,gaussian=False):
         self.noisy_X = X
         self.K = K
         self.N = X.shape[0]
+        self.P = X.shape[1]
         self.num_runs = num_runs
         self.niter = niter
+        self.gaussian = gaussian
+        self.C = np.eye(self.K+1) - 1/(self.K+1)*np.ones((self.K+1,self.K+1))
         
     def run(self):
+        L = defaultdict()
         L_temp = self.noisy_X
-        lambdas = [1]*self.N
+        lambdas = [100000]*self.N
         for i in range(self.num_runs):
             print(f'Start round {i} of NRPCA ...')
             A,patch,num_copies = self.distance_matrix(L_temp)
-            L_temp = self.solver(patch,num_copies)
-            L.concatenate(L_temp,axis=1)
+            L_temp = self.solver(patch,num_copies,lambdas)
+            L[i] = L_temp
+        if self.gaussian:
+            X_hat = self.clean_L(self.noisy_X-L_temp,patch,num_copies)
+        else:
+            X_hat = L_temp
+        return L,X_hat
+    
+    def solver(self,patch,num_copies,lambdas):
+        print('Start solving minimization problem...')
+        #S = np.random.randn(self.noisy_X.shape[0],self.noisy_X.shape[1])
+        S = np.zeros_like(self.noisy_X)
+        t = 0.005
+        theta = 1
+        S_old = S
+        for i in range(self.niter):
+            print(f'Iter {i}')
+            S_new = self.g_prox(S - t * self.f_grad(S,patch,lambdas),t,num_copies)
+            theta_new = 0.5 * (1 + np.sqrt(1 + 4 * theta**2))
+            S = S_new + (theta - 1.)/theta_new * (S_new - S_old)
+            S_old = S_new
+            theta = theta_new
+            obj_val = self.f_val(S_new,patch,lambdas) + self.g_val(S_new,num_copies)
+            print(f'Objective value: {obj_val}')         
+        L = self.noisy_X - S_new
         return L
-            
+    
     def distance_matrix(self,X):
-        D = sp.spatial.distance.cdist(X,X)
-        num_copies = Counter()
+        D = cdist(X,X)
+        num_copies = defaultdict(lambda:1)
         patch = {i:[i] for i in range(self.N)}
-        D1 = np.zeros((self.N,self.K))
+        D1 = np.zeros_like(D)
         for i in range(self.N):
-            indices = np.argsort(D[i])[:K+1]
+            indices = np.argsort(D[i])[:self.K+1]
             for j in indices:
                 if j != i:
-                    D[i,j] = 1
+                    D1[i,j] = 1
                     num_copies[j] += 1
                     patch[i].append(j)
         A = D*D1
         return A,patch,num_copies
+        
+    def f_grad(self,S,patch,lambdas):
+        y = np.zeros_like(self.noisy_X)
+        for i in range(self.N):
+            neighbors = patch[i]
+            S_i = self.C@S[neighbors]
+            X_i = self.C@self.noisy_X[neighbors]
+            [U,Sigma_i,Vh] = np.linalg.svd(X_i-S_i)
+            #Sigma_i = [max(x-1/(2*lambdas[i]),0) for x in Sigma_i]
+            Sigma_i = [1]*len(Sigma_i)
+            L_i = np.dot(U,np.dot(sp.linalg.diagsvd(Sigma_i,*X_i.shape),Vh))
+            #temp = 2 * lambdas[i] * (L_i + S_i - X_i)
+            temp = -self.C@L_i 
+            y[neighbors] += temp
+        return y
     
-    def solver(self,patch,num_copies):
-        print('Start solving minimization problem...
+    def f_val(self,S,patch,lambdas):
+        y = 0
+        for i in range(self.N):
+            neighbors = patch[i]
+            S_i = self.C@S[neighbors]
+            X_i = self.C@self.noisy_X[neighbors]
+            [U,Sigma_i,Vh] = np.linalg.svd(X_i-S_i)
+            tmp = sum(Sigma_i)
+            #Sigma_i = [max(x-1/(2*lambdas[i]),0) for x in Sigma_i]
+            #L_i = np.dot(U,np.dot(sp.linalg.diagsvd(Sigma_i,*X_i.shape),Vh))
+            #y += lambdas[i] * np.linalg.norm(X_i-L_i-S_i,'fro') + tmp
+            y += tmp
+        return y
+    
+    def g_prox(self,S,t,num_copies):
+        mu = np.array([num_copies[i]/np.sqrt(max(self.K,self.P)) for i in range(self.N)]).reshape(-1,1)
+        y = np.maximum(0,S-t*mu*np.ones((1,self.P))) - np.maximum(0,-S-t*mu*np.ones((1,self.P)))
+        return y
+    
+    def g_val(self,S,num_copies):
+        y = 0
+        mu = [num_copies[i]/np.sqrt(max(self.K,self.P)) for i in range(self.N)]
+        for i in range(self.N):
+            y += mu[i] * sum(abs(S[i]))
+        return y
+
+    def optimal_thresh(self):
+        beta = min(self.P/self.K, self.K/self.P);
+        return np.sqrt(2*(beta+1)+8*beta/((beta+1)+np.sqrt(beta**22+14*beta+1)))
+    
+    def clean_L(self,S,patch,num_copies):
+        X_hat = np.zeros_like(self.noisy_X)
+        thresh = self.optimal_thresh()
+        for i in range(self.N):
+            neighbors = patch[i]
+            S_i = S[neighbors]
+            X_i = self.noisy_X[neighbors]
+            [U,Sigma_i,Vh] = np.linalg.svd(self.C@(X_i-S_i))
+            Sigma_i[Sigma_i < thresh] = 0
+            L_i = np.dot(U,np.dot(sp.linalg.diagsvd(Sigma_i,*X_i.shape),Vh)) + (np.eye(self.K+1)-self.C)@(X_i-S_i)
+            X_hat[neighbors] += L_i
+        for i in range(self.N):
+            X_hat[i] = [x/num_copies[i] for x in X_hat[i]]
+        return X_hat
+            
+        
+def parse_args():
+    parser = argparse.ArgumentParser(description="Run NRPCA.")
+    parser.add_argument('-data', type=str, default='Swiss', help='Swiss or MNIST')
+    parser.add_argument('-N', type=int, default=2000, help='Number of datapoints')
+    parser.add_argument('-K', type=int, default=15, help='Number of neighbors')
+    parser.add_argument('-Sparse_noise_level', type=float, default=6., help='Sparse noise level, only for swiss')
+    parser.add_argument('-Gaussian_noise_level', type=float, default=0.5,help='Gaussian noise level, only for swiss')
+    parser.add_argument('-num_runs', type=int, default=2,help='Number of updating neighbors')
+    parser.add_argument('-niter', type=int, default=100,help='Number of iters')
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+    N,K,sparse_noise_level,gaussian_noise_level = args.N,args.K,args.Sparse_noise_level,args.Gaussian_noise_level
+    num_runs,niter = args.num_runs,args.niter
+    if args.data.startswith('Swiss'):
+        X,clean_X,color = get_swiss(N,sparse_noise_level=sparse_noise_level,gaussian_noise_level=gaussian_noise_level)
+    elif args.data.startswith('MNIST'):
+        X = get_mnist(N)
+        clean_X,color = None,None
+    
+    method = NRPCA(X,K,num_runs,niter,gaussian=gaussian_noise_level>0)
+    L,X_hat = method.run()
+    return X,L,X_hat,clean_X,color
+
+if __name__ == '__main__':
+    return main()
+            
+            
+            
 
--- utils.py
+++ utils.py
@@ -1,8 +1,7 @@
 import numpy as np
 from random import choices
 import math
-from scipy.spatial.distance import cdist
-from collections import Counter
+import random
 
 def get_swiss(N,sparse_noise_level,gaussian_noise_level=0):
     # Construct swiss roll of dim 20
@@ -14,33 +13,22 @@ def get_swiss(N,sparse_noise_level,gaussian_noise_level=0):
     z = 8*math.pi*np.random.rand(N,1)
 
     t_max = np.max(t)**2+1
-    c = 256/t_max*(t**2+1)
+    color = 256/t_max*(t**2+1)
     X = np.concatenate((x,y,z),axis=1)
     for d in range(4,21):
         tmp = t*np.sin(d*t/21).reshape(N,1)
         X = np.concatenate((X,tmp),axis=1)
+    clean_X = X.copy()
     # Add sparse noise
-    num_outliers = math.ceil(N*P*0.015)
+    num_outliers = math.ceil(N*P*0.05)
     outlier_loc = choices(list(np.arange(N)),k=num_outliers)
-    outlier_dim = choices(list(np.arange(2))+list(np.arange(3,P+1)),k=num_outliers)
+    outlier_dim = choices(list(np.arange(2))+list(np.arange(3,P)),k=num_outliers)
     for r,c in zip(outlier_loc,outlier_dim):
         X[r][c] += np.random.normal(sparse_noise_level,0.03)*int(random.random()<0.5)
     # Add gaussian noise
     X += gaussian_noise_level*np.random.randn(N,20)
-    return X
+    return X,clean_X,color
 
-def distance_matrix(X,K):
-    D = sp.spatial.distance.cdist(X,X)
-    N = D.shape[0]
-    num_copies = Counter()
-    D1 = np.zeros((N,K))
-    for i in range(N):
-        indices = np.argsort(D[i])[:K+1]
-        for j in indices:
-            if j != i:
-                D[i,j] = 1
-                num_copies[j] += 1
-    A = D*D1
-    return A,num_copies
+
         
         
